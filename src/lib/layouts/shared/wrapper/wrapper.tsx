e80a07b787230d3f3cc9f21cd49f1ddaeb3cc623
Gustavo Morinaga <me@gustavomorinaga.dev>
2025-03-26 21:41:34 -0300

    feat: add background color to wrapper component for improved UI consistency

--- src/lib/layouts/shared/wrapper/wrapper.tsx
+++ src/lib/layouts/shared/wrapper/wrapper.tsx
@@ -9,7 +9,10 @@ type TWrapperProps = React.ComponentProps<'div'>;
  */
 export function Wrapper({ className, children, ...props }: TWrapperProps) {
 	return (
-		<div className={cn('flex size-full min-h-svh flex-1 flex-col', className)} {...props}>
+		<div
+			className={cn('bg-background flex size-full min-h-svh flex-1 flex-col', className)}
+			{...props}
+		>
 			{children}
 		</div>
 	);
